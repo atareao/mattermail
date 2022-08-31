a87dcf103f3caeb4165bf83dba986f5b890d6f75
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-08-31 07:22:04 +0200

    http

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
-use actix_web::{App, HttpServer, web::Data, dev::ServiceRequest, Error};
+use actix_web::{App, HttpServer};
 
 mod routes;
 
-use routes::{root};
+use routes::{root, status, hook};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(move ||{
         App::new()
             .service(root)
+            .service(status)
+            .service(hook)
     })
         .bind("127.0.0.1:8080")
         .unwrap()
--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,33 @@
-use actix_web::{get, post, Error, HttpResponse, http::StatusCode};
+use actix_web::{get, post, Error, HttpResponse, http::StatusCode, http::header::ContentType};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Respuesta{
+    code: i32,
+    status: String,
+    message: String,
+}
 
 
 #[get("/")]
 pub async fn root() -> Result<HttpResponse, Error>{
-    Ok(HttpResponse::build(StatusCode::OK).body("Hola mundo"))
+    Ok(HttpResponse::build(StatusCode::OK).body("Rust is the best!"))
+}
+
+#[get("/status")]
+pub async fn status() -> Result<HttpResponse, Error>{
+    let respuesta = Respuesta{
+        code: 200,
+        status: "Ok".to_string(),
+        message: "Up and running!".to_string(),
+    };
+    Ok(HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(serde_json::to_string(&respuesta)?))
+}
+
+#[post("/hook")]
+pub async fn hook(post: String) -> Result<HttpResponse, Error>{
+    println!("{}", post);
+    Ok(HttpResponse::build(StatusCode::OK).body(format!("Message recieved {}", post)))
 }
