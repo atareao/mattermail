32251b7194f268acb237c2a4faf10e292dbcb143
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-09-14 07:00:55 +0200

    environment to ignore

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,29 @@
-
-use actix_web::{App, HttpServer};
-
 mod routes;
 mod mail;
 
-use routes::{root, status, hook};
 use crate::mail::get_unread_mails;
 use std::{thread, time};
+use dotenv::dotenv;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut stop: &'static bool = false;
-    let duration = &time::Duration::from_secs_f32(300.0);
-    let handle = thread::spawn(|| async {
-        while !stop{
-            let mails = get_unread_mails("imap.atareao.es", 995, "atareao@atareao.es", "secreto").await;
-            for mail in mails{
-                println!("{}", mail);
-            }
-            thread::sleep(*duration);
+    dotenv().ok();
+    let imap_host = env::var("HOST").expect("HOST not set");
+    let imap_port: u16 = env::var("PORT").expect("PORT not set")
+        .parse()
+        .unwrap();
+    let imap_login = env::var("LOGIN").expect("LOGIN not set");
+    let imap_passwd = env::var("PASSWD").expect("PASSWD not set");
+    let mut stop = false;
+    let duration = time::Duration::from_secs_f32(300.0); // 5 minutes
+    while !stop{
+        let mails = get_unread_mails(&imap_host, imap_port, &imap_login, &imap_passwd).await;
+        for mail in mails{
+            println!("{}", mail);
         }
-    });
-
+        thread::sleep(duration);
+    }
     stop = true;
-    HttpServer::new(move ||{
-        App::new()
-            .service(root)
-            .service(status)
-            .service(hook)
-    })
-        .bind("0.0.0.0:8080")
-        .unwrap()
-        .run()
-        .await
-
+    Ok(())
 }
