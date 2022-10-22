a34a6d0b155f85ba088a1ac893484426e816751b
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-10-22 20:54:30 +0200

    add mattermost and send

--- src/mail.rs
+++ src/mail.rs
@@ -1,4 +1,3 @@
-use actix_web::body::MessageBody;
 use async_native_tls::TlsConnector;
 //use futures_util::
 use futures::TryStreamExt;
@@ -6,32 +5,33 @@ use async_imap;
 use std::fmt::Display;
 use html2md::parse_html;
 use std::fmt;
-use email_parser::{email::Email, address::Mailbox};
+use mail_parser::{Message, HeaderValue, Addr, Group};
 
 pub struct Mail{
+    id: u32,
+    reg: String,
     from: String,
     subject: String,
-    content: String,
 }
 
 impl Mail {
-    pub fn new(message: &Email) -> Self{
-        let from = get_addresses(&message.from);
-        let subject = message.subject.as_ref().unwrap().to_string();
-        let content = parse_html(&message.body.as_ref().unwrap().to_string());
+    pub fn new(id: u32, content: &Message) -> Self{
+        let reg = content.get_message_id().unwrap().to_string();
+        let from = get_address(&content.get_from());
+        let subject = content.get_subject().unwrap_or("").to_string();
         Self{
+            id,
+            reg,
             from,
             subject,
-            content,
         }
-
     }
 }
 
 impl Display for Mail {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "From: {}\nSubject: {}\nContent: {}",
-            self.from, self.subject, self.content)
+        write!(f, "Id: {}\nReg: {}\nFrom: {}\nSubject: {}",
+            self.id, self.reg, self.from, self.subject)
     }
 }
 
@@ -45,50 +45,59 @@ pub async fn get_unread_mails(server: &str, port: u16, user: &str, password: &st
         .map_err(|e| e.0).unwrap();
     imap_session.select("INBOX").await.unwrap();
     let mut new_items = imap_session.search("NOT SEEN").await.unwrap();
-    println!("===============");
-    let first = new_items.drain().next().unwrap().to_string();
-    let messages_stream = imap_session.fetch(first, "RFC822").await.unwrap();
-    let messages: Vec<_> = messages_stream.try_collect().await.unwrap();
-    let mut counter = 0;
-    for message in messages {
-        let body = message.body().unwrap();
-        match Email::parse(body) {
-            Ok(content) => result.push(Mail::new(&content)),
-            Err(e) => println!("{:?}", e),
-        }
+    match new_items.drain().next(){
+        Some(identificador) => {
+            let messages_stream = imap_session.fetch(identificador.to_string(), "RFC822").await.unwrap();
+            let messages: Vec<_> = messages_stream.try_collect().await.unwrap();
+            for message in messages {
+                let id = message.message;
+                let body = message.body().unwrap();
+                match Message::parse(body){
+                    Some(content) => result.push(Mail::new(id, &content)),
+                    None => println!("{:?}", body),
+                }
+            }
+        },
+        None => {}
     }
-    /*
-    for item in new_items {
-        println!("{}", item);
-        let messages_stream = imap_session.fetch(item.to_string(), "RFC822").await.unwrap();
-        let messages: Vec<_> = messages_stream.try_collect().await.unwrap();
-        for message in messages {
-            //let envelope = message.envelope().unwrap();
-            //let subject = get_from_cow(&envelope.subject);
-            ////let subject = unsafe {std::str::from_utf8_unchecked(&envelope.subject.as_ref().unwrap())};
-            //let from = &envelope.from;
-            let body = message.body().unwrap();
-            result.push(parse_html(&std::str::from_utf8(body).unwrap().to_string()));
-        }
-    }
-        */
-    println!("===============");
     result
 }
 
-fn get_addresses(mailboxes: &Vec<Mailbox>) -> String{
-    let mut addresses = Vec::new();
-    for mailbox in mailboxes{
-        addresses.push(get_address(mailbox));
+fn get_from_groups(groups: &Vec<Group>) -> String{
+    let mut result = Vec::new();
+    for group in groups{
+        result.push(get_from_addresses(&group.addresses));
     }
-    addresses.join(", ")
+    result.join(", ")
 }
 
-fn get_address(mailbox: &Mailbox) -> String{
-    let user_name = match &mailbox.name{
-        Some(names) => names.join(" "),
+fn get_from_addresses(addresses: &Vec<Addr>) -> String{
+    let mut result = Vec::new();
+    for address in addresses{
+        result.push(get_from_address(address))
+    }
+    result.join(", ")
+}
+fn get_from_address(address: &Addr) -> String{
+    let name = match &address.name{
+        Some(name) => name.to_string(),
         None => "".to_string(),
     };
-    format!("{} <{}@{}>", user_name, mailbox.address.local_part,
-        mailbox.address.domain)
+    let mail = match &address.address{
+        Some(mail) => mail.to_string(),
+        None => "".to_string(),
+    };
+    format!("{} <{}>", name, mail)
+}
+
+fn get_address(header: &HeaderValue) -> String{
+    match header {
+        HeaderValue::Address(address) => get_from_address(address),
+        HeaderValue::AddressList(addresses) => get_from_addresses(addresses),
+        HeaderValue::Text(text) => text.to_string(),
+        HeaderValue::TextList(textlist) => textlist.join(", "),
+        HeaderValue::Group(group) => get_from_addresses(&group.addresses),
+        HeaderValue::GroupList(groups) => get_from_groups(groups),
+        _ => "".to_string(),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,36 @@
 mod routes;
 mod mail;
+mod mattermost;
 
 use crate::mail::get_unread_mails;
 use std::{thread, time};
 use dotenv::dotenv;
+use mattermost::Mattermost;
 use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    let pull_time: u64 = env::var("PULL_TIME").unwrap_or("300".to_string())
+        .parse()
+        .unwrap();
     let imap_host = env::var("HOST").expect("HOST not set");
     let imap_port: u16 = env::var("PORT").expect("PORT not set")
         .parse()
         .unwrap();
     let imap_login = env::var("LOGIN").expect("LOGIN not set");
     let imap_passwd = env::var("PASSWD").expect("PASSWD not set");
-    let mut stop = false;
-    let duration = time::Duration::from_secs_f32(300.0); // 5 minutes
-    while !stop{
+    let base_uri = env::var("MATTERMOST_BASE_URI").expect("MATTERMOST_BASE_URI not set");
+    let token = env::var("MATTERMOST_ACCESS_TOKEN").expect("MATTERMOST_ACCESS_TOKEN not set");
+    let duration = time::Duration::from_secs(pull_time); // 5 minutes
+    let mm = Mattermost::new(&base_uri, &token);
+    let channel_id = mm.get_channel_by_name("correo").await.unwrap();
+    loop {
         let mails = get_unread_mails(&imap_host, imap_port, &imap_login, &imap_passwd).await;
         for mail in mails{
             println!("{}", mail);
+            mm.post_message(&channel_id, &mail.to_string(), None).await.unwrap();
         }
         thread::sleep(duration);
     }
-    stop = true;
-    Ok(())
 }
